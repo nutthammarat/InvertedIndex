3be3088f9668d11130e44f49b663fa3cabbf5973
nutthammarat <59161088@go.buu.ac.th>
2019-01-17 13:52:40 +0700

    add 3 Doc

--- src/InvetedIndex.java
+++ src/InvetedIndex.java
@@ -5,22 +5,25 @@ import java.io.FileReader;
 import java.util.stream.Collectors;
 
 public class InvetedIndex {
-    private static String[] token;
-    private static String[] tokens;
+    public static String[] token;
+    public static String[] tokens;
+    public static String[] tokenss;
     private static ArrayList<String> text = new ArrayList<>();
     private static ArrayList<String> texts = new ArrayList<>();
+    private static ArrayList<String> textss = new ArrayList<>();
     private static ArrayList<Token> tokenx = new ArrayList<>();
     private static LinkedList<String> keep = new LinkedList<>();
     private static String find;
     private static String address;
     private static String address2;
-
+    private static String delim = "\\ |\\;|\\:|\\.|\\,|\\-";
     public static void main(String[] args) throws Exception {
         Scanner kb = new Scanner(System.in);
-        find = kb.next();
-        System.out.println("Word : "+find);
+
         if (readFile()) {
-            addTextToLinkedLists(token, tokens);
+//            find = kb.next();
+//            System.out.println("Word : "+find);
+            addTextToLinkedLists(token, tokens,tokenss);
             Collections.sort(tokenx, Token.sortAllInfo);
             removeSameWordinTokenx();
             printDetail(find);
@@ -32,45 +35,47 @@ public class InvetedIndex {
     }
 
     public static void printDetail(String find) {
-//        for (Tokenx texts : tokenx) {
-//            System.out.println(texts);
-//
-//
-//        }
-        boolean flag = false;
-        for(int i=0;i<tokenx.size();i++){
-            if(tokenx.get(i).getTerm().equals(find)){
-                flag = true;
-                if(tokenx.get(i).getDocID().equals("1")){
-                    System.out.println(address);
-                 }
-                else if(tokenx.get(i).getDocID().equals("2")){
-                    System.out.println(address2);
-                 }else{
-                    System.out.println(address + "\n" +address2);
-                }
-            }
+        for (Token texts : tokenx) {
+            System.out.println(texts);
+
+
         }
-        if(!flag)
-            System.out.println("No word on any Doc!!");
+        boolean flag = false;
+//        for(int i=0;i<tokenx.size();i++){
+//            if(tokenx.get(i).getTerm().equals(find)){
+//                flag = true;
+//                if(tokenx.get(i).getDocID().equals("1")){
+//                    System.out.println(address);
+//                 }
+//                else if(tokenx.get(i).getDocID().equals("2")){
+//                    System.out.println(address2);
+//                 }else{
+//                    System.out.println(address + "\n" +address2);
+//                }
+//            }
+//        }
+//        if(!flag)
+//            System.out.println("No word on any Doc!!");
     }
 
     public static boolean readFile() {
         try {
-            File file = new File("C:\\Users\\59161088\\Desktop\\Doc1.txt");
-            File files = new File("C:\\Users\\59161088\\Desktop\\Doc2.txt");
+            File file = new File("C:\\Users\\59161088\\Desktop\\Doc\\Doc1.txt");
+            File files = new File("C:\\Users\\59161088\\Desktop\\Doc\\Doc2.txt");
+            File filess = new File("C:\\Users\\59161088\\Desktop\\Doc\\Doc3.txt");
             address = file.toString();
             address2 = files.toString();
-            extractToToken(file, files);
+            extractToToken(file, files,filess);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
 
-    public static void extractToToken(File file, File files) throws Exception {
+    public static void extractToToken(File file, File files,File filess) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(file));
         BufferedReader brs = new BufferedReader(new FileReader(files));
+        BufferedReader brss = new BufferedReader(new FileReader(filess));
 //        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
 //        StringTokenizer stringTokenizers = new StringTokenizer(brs.readLine());
 //        while(stringTokenizer.hasMoreElements()){
@@ -79,23 +84,30 @@ public class InvetedIndex {
 //        while(stringTokenizers.hasMoreElements()){
 //            texts.add(stringTokenizers.nextElement().toString());
 //        }
-        token = br.readLine().split(" ");
-        tokens = brs.readLine().split(" ");
+        token = br.readLine().split(delim);
+        tokens = brs.readLine().split(delim);
+        tokenss = brss.readLine().split(delim);
     }
 
-    public static void addTextToLinkedLists(String[] token, String[] tokens) {
+    public static void addTextToLinkedLists(String[] token, String[] tokens,String[] tokenss) {
         for (String t : token) {
-            text.add(t.toLowerCase().toString());
+            text.add(t.toLowerCase());
         }
         for (String t : tokens) {
-            texts.add(t.toLowerCase().toString());
+            texts.add(t.toLowerCase());
+        }
+        for (String t : tokenss) {
+            textss.add(t.toLowerCase());
         }
         removeDuplicates();
         for (String t : text) {
-            tokenx.add(new Token(t.toLowerCase(), "1"));
+            tokenx.add(new Token(t, "1"));
         }
         for (String t : texts) {
-            tokenx.add(new Token(t.toLowerCase(), "2"));
+            tokenx.add(new Token(t, "2"));
+        }
+        for (String t : textss) {
+            tokenx.add(new Token(t, "3"));
         }
     }
 
---
+++ src/StorePath.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class StorePath {
+    public static String doc1 = "C:\\Users\\59161088\\Desktop\\Doc\\Doc1.txt";
+    public static String doc2 = "C:\\Users\\59161088\\Desktop\\Doc\\Doc2.txt";
+    public static String doc3 = "C:\\Users\\59161088\\Desktop\\Doc\\Doc3.txt";
+    public static String doc4 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc5 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc6 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc7 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc8 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc9 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc10 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc11 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc12 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc13 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc14 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc15 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc16 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc17 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc18 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc19 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc20 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc21 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc22= "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc23 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc24 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc25 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc26 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc27 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc28 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc29 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc30 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc31 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc32 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc33 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc34 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc35 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc36 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc37 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc38 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc39 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc40 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc41 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc42 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc43 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc44 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc45 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc46 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc47 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc48 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc49 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc50 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc51 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc52 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc53 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc54 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc55 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc56 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc57 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc58 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc59 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc60 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc61 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc62 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc63 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc64 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc65 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc66 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc67 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc68 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc69 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc70 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc71 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc72= "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc73 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc74 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc75 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc76 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc77 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc78 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc79 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc80 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc81 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc82 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc83 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc84 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc85 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc86 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc87 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc88 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc89 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc90 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc91 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc92 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc93 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc94 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc95 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc96 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc1.txt";
+    public static String doc97 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc2.txt";
+    public static String doc98 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc99 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+    public static String doc100 = "C:\\\\Users\\\\59161088\\\\Desktop\\\\Doc3.txt";
+//    private static File file;
+//    private static File files;
+//    private static File filess;
+//    public static boolean readFile() {
+//        try {
+//            file = new File(doc1);
+//            files = new File(doc2);
+//            filess = new File(doc3);
+//
+//            extractToToken();
+//            return true;
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
+//
+//    public static void extractToToken() throws Exception {
+//        BufferedReader br = new BufferedReader(new FileReader(file));
+//        BufferedReader brs = new BufferedReader(new FileReader(files));
+//        BufferedReader brss = new BufferedReader(new FileReader(files));
+//        InvetedIndex.token = br.readLine().split(InvetedIndex.delim);
+//        InvetedIndex.tokens = brs.readLine().split(InvetedIndex.delim);
+//        InvetedIndex.tokenss = brss.readLine().split(InvetedIndex.delim);
+//    }
+}
